b188df4c3d5657f4eeb092ef5e8a556e6aa2a240
Rheisen Dennis <dennis.rheisen@gmail.com>
2018-10-15 10:50:01 -0400

    complete desktop home page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import menuIcon from '../../assets/icons/menu-icon.png';
+
 import '../../styles/components/header.scss';
 
 const Header = () => (
   <header className="component-header__header__wrapper">
     <h1 className="component-header__h1__temp-brand-mark">blend</h1>
+    <nav className="component-header__nav__navigation-page">
+      <img
+        src={menuIcon}
+        alt="Navigation"
+        className="component-header__img__icon-navigation"
+      />
+    </nav>
   </header>
 );
 
